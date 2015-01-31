3ef4c3130879b86c3d35c87541f4005f8845cc3a
Black-Hole1 <root@freeattack.com>
2015-01-31 14:00:16 +0800

    第六次更新
    修复了两处bug，修复了兼容问题，更改了css和js的命名。添加了A标签按钮组，下次更新为button标签按钮组和其他样式

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# flawless是一个由less开发的一个css框架,使用了Glyphicons字体图标,而Glyphicons字体图标来自于bootstrap.
-## flawless面对的是中国地区的开发者,国外可能不太适应呢~
+## flawless是一个由less开发的一个css框架,使用了Glyphicons字体图标,而Glyphicons字体图标来自于bootstrap.
+### flawless面对的是中国地区的开发者,国外可能不太适应呢~
+### 不兼容IE，虽说IE下能看，但是不美观。等IE退出浏览器平台时，我一定会开瓶红酒庆祝
+### 因为使用的是CSS3，所以可能在不同浏览器下，展现的方式不同，我也在努力做到每个浏览器下，展现的页面都是十分漂亮的
 
-### 使用方法：
-在文件里调用flawless.css和bootstrap_ico.css就行了。
+## 使用方法：
+在文件里调用flawless.css和base.js(事先要调用jquery)就行了。
 如果你想更改,可以在less/flawless.less里修改下include文件就可以完成自定义哦~
--- js/base.js
+++ js/base.js
@@ -1,15 +1,15 @@
 //a标签脚本
 $(document).ready(function() {
-	var a_tag_length = $(".a_tag-link").length;
-	var a_tag_href = $(".a_tag-link").attr("href");
-	var a_tag_text = $(".a_tag-link").text();
+	var a_tag_length = $(".a-tag-link").length;
+	var a_tag_href = $(".a-tag-link").attr("href");
+	var a_tag_text = $(".a-tag-link").text();
 	$(".a_tag-link").attr("title", a_tag_href);
 	for (var i = 0; i < a_tag_length; i++) {
-		if ($(".a_tag-link:eq(" + i + ")").text() !== "") {
-			$(".a_tag-link:eq(" + i + ")").text("");
-			$(".a_tag-link:eq(" + i + ")").append('<i class="glyphicon glyphicon-link"></i><i class="division">|</i><em>' + a_tag_text + '</em>');
+		if ($(".a-tag-link:eq(" + i + ")").text() !== "") {
+			$(".a-tag-link:eq(" + i + ")").text("");
+			$(".a-tag-link:eq(" + i + ")").append('<i class="glyphicon glyphicon-link"></i><i class="division">|</i><em>' + a_tag_text + '</em>');
 		} else {
-			$(".a_tag-link:eq(" + i + ")").append('<i class="glyphicon glyphicon-link"></i><i class="division">|</i><em>网页外链</em>');
+			$(".a-tag-link:eq(" + i + ")").append('<i class="glyphicon glyphicon-link"></i><i class="division">|</i><em>网页外链</em>');
 		}
 	}
 });
@@ -18,7 +18,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	$("button").each(function(){
 		var button_tag_data_text = $(this).attr("data");
-		if(button_tag_data_text !== undefined && $(this).hasClass('button_tag')){
+		if(button_tag_data_text !== undefined && $(this).hasClass('button-tag')){
 			$(this).css({
 				"height":button_tag_data_text,
 				"width":button_tag_data_text*1+3
@@ -26,13 +26,14 @@ $(document).ready(function() {
 		}
 	});
 });
+
 //input脚本
 $(document).ready(function() {
-	$(".form_tag input[data='email'] , .form_tag input[data='邮箱']").attr("placeholder", "邮箱/Email");
-	$(".form_tag input[data='user'], .form_tag input[data='username'] , .form_tag input[data='用户'] , .form_tag input[data='用户名'] , .form_tag input[data='登录名']").attr("placeholder", "用户名/UserName");
-	$(".form_tag input[type='password']").attr("placeholder", "密码/PassWord");
+	$(".form-tag input[data='email'] , .form-tag input[data='邮箱']").attr("placeholder", "邮箱/Email");
+	$(".form-tag input[data='user'], .form-tag input[data='username'] , .form-tag input[data='用户'] , .form-tag input[data='用户名'] , .form-tag input[data='登录名']").attr("placeholder", "用户名/UserName");
+	$(".form-tag input[type='password']").attr("placeholder", "密码/PassWord");
 	var data_arr = ["email", "邮箱", "user", "username", "用户", "用户名", "登录名", "password"];
-	$(".form_tag input").each(function() {
+	$(".form-tag input").each(function() {
 		var a_tag_data_text = $(this).attr("data");
 		if(a_tag_data_text == undefined && ($(this).attr("type") == "text" || $(this).attr("type") == undefined)){
 			$(this).attr("placeholder", "搜索/Search");
@@ -57,7 +58,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	$("code").each(function() {
 		if (!$(this).hasClass('code-fragment')) {
-			$(this).addClass('code_tag');
+			$(this).addClass('code-tag');
 		}
 	});
 });
