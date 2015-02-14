08103e106ed0850da51d61019cb0161cf7252a24
Black-Hole1 <root@freeattack.com>
2015-02-14 20:31:08 +0800

    第十二次更新
    优化了a标签按钮组和button按钮组的不可点状态。修复了导航栏的优先级(li>ul:z-index:1)，增加了“消息窗口”“面板公告”“面包屑导航”

--- js/base.js
+++ js/base.js
@@ -19,17 +19,29 @@ $(document).ready(function(){
 	})
 });
 
+//面包屑脚本
+$(document).ready(function(){
+	$(".crumbs li").each(function(){
+		if($(this).next("li").length == 0){
+			$(this).find("a").attr("href","javascript:void(0)");
+		}
+		// if($(this).next("li") == undefined){
+		// 	$(this).find("a").attr("href","javascript:void(0)");
+		// }
+	})
+})
+
 //a标签脚本
 $(document).ready(function() {
-	var a_tag_href = $(".a-tag-link").attr("href");
-	$(".a_tag-link").attr("title", a_tag_href);
-	var a_tag_text = $(".a-tag-link").text();
 	$(".a-tag-link").each(function(){
-		if($(this).text() !== ""){
+		var a_tag_href = $(this).attr("href");
+		$(this).attr("title","URL: " + a_tag_href);
+		if($(this).text() == ""){
+			$(this).append('<i class="glyphicon glyphicon-link"></i><i class="division">|</i><em>网页外链</em>');
+		}else{
+			var a_tag_text = $(this).text();
 			$(this).text("");
 			$(this).append('<i class="glyphicon glyphicon-link"></i><i class="division">|</i><em>' + a_tag_text + '</em>');
-		}else{
-			$(this).append('<i class="glyphicon glyphicon-link"></i><i class="division">|</i><em>网页外链</em>');
 		}
 	})
 })
@@ -49,21 +61,36 @@ $(document).ready(function() {
 
 //按钮组不可点脚本
 $(document).ready(function(){
+	var a_tag_group_length = $(".a-tag-group a").length;
+	$(".a-tag-group").prepend('<a href="#" class="last">&lt;&lt;</a>').append('<a href="#" class="last">&gt;&gt;</a>');
 	$(".a-tag-group a").each(function(){
 		if($(this).hasClass("new")){
 			$(this).attr("href","javascript:void(0)");
 		}
 	})
+	if($(".a-tag-group a").eq(1).hasClass("new")){
+		 $(".a-tag-group a").eq(0).addClass("no-click no-hover").attr("href","javascript:void(0)");
+	}
+	if($(".a-tag-group a").eq(a_tag_group_length).hasClass("new")){
+		$(".a-tag-group a").last().addClass("no-click no-hover").attr("href","javascript:void(0)");
+	}
 
-
+	var button_tag_group_length = $(".button-tag-group button").length;
+	$(".button-tag-group").prepend('<button class="last">&lt;&lt;</button>').append('<button class="last">&gt;&gt;</button>');
 	$(".button-tag-group button").each(function(){
 		if($(this).hasClass("new")){
 			$(this).attr("disabled",true);
 		}
 	})
+	if($(".button-tag-group button").eq(1).hasClass("new")){
+	 	$(".button-tag-group button").eq(0).addClass("no-click no-hover").attr("disabled",true);
+	 }
+	if($(".button-tag-group button").eq(button_tag_group_length).hasClass("new")){
+		$(".button-tag-group button").last().addClass("no-click no-hover").attr("disabled",true);
+	}
 })
 
-//input脚本
+//input脚本 例子1
 $(document).ready(function() {
 	$(".form-tag input[data='email'] , .form-tag input[data='邮箱']").attr("placeholder", "邮箱/Email");
 	$(".form-tag input[data='user'], .form-tag input[data='username'] , .form-tag input[data='用户'] , .form-tag input[data='用户名'] , .form-tag input[data='登录名']").attr("placeholder", "用户名/UserName");
@@ -88,13 +115,14 @@ $(document).ready(function() {
 			$(this).css("height",input_height);
 		}
 	})
-})
+});
 
-//code样式
+
+// code样式
 $(document).ready(function() {
 	$("code").each(function() {
-		if (!$(this).hasClass('code-fragment')) {
+		if (!$(this).hasClass('code-fragment')){
 			$(this).addClass('code-tag');
 		}
 	})
-})
+});
