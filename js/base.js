a91ff84ed8884306aba243dcf50e885716082b57
Black-Hole1 <root@freeattack.com>
2015-02-19 14:49:33 +0800

    第十三次更新
    增加了3个footer样式供选择，增加了input组，修复了img圆角在UC下不支持。增加了面包屑导航的Icon。增加了index.html可本地搭建环境环境(asp、php、jsp都行，支持html就可以。注意环境必须是根目录下，因为在index.html下使用的是/绝对目录)

--- js/base.js
+++ js/base.js
@@ -61,32 +61,32 @@ $(document).ready(function() {
 
 //按钮组不可点脚本
 $(document).ready(function(){
-	var a_tag_group_length = $(".a-tag-group a").length;
-	$(".a-tag-group").prepend('<a href="#" class="last">&lt;&lt;</a>').append('<a href="#" class="last">&gt;&gt;</a>');
-	$(".a-tag-group a").each(function(){
+	var a_page_length = $(".a-page a").length;
+	$(".a-page").prepend('<a href="#" class="last">&lt;&lt;</a>').append('<a href="#" class="last">&gt;&gt;</a>');
+	$(".a-page a").each(function(){
 		if($(this).hasClass("new")){
 			$(this).attr("href","javascript:void(0)");
 		}
 	})
-	if($(".a-tag-group a").eq(1).hasClass("new")){
-		 $(".a-tag-group a").eq(0).addClass("no-click no-hover").attr("href","javascript:void(0)");
+	if($(".a-page a").eq(1).hasClass("new")){
+		 $(".a-page a").eq(0).addClass("no-click no-hover").attr("href","javascript:void(0)");
 	}
-	if($(".a-tag-group a").eq(a_tag_group_length).hasClass("new")){
-		$(".a-tag-group a").last().addClass("no-click no-hover").attr("href","javascript:void(0)");
+	if($(".a-page a").eq(a_page_length).hasClass("new")){
+		$(".a-page a").last().addClass("no-click no-hover").attr("href","javascript:void(0)");
 	}
 
-	var button_tag_group_length = $(".button-tag-group button").length;
-	$(".button-tag-group").prepend('<button class="last">&lt;&lt;</button>').append('<button class="last">&gt;&gt;</button>');
-	$(".button-tag-group button").each(function(){
+	var button_page_length = $(".button-page button").length;
+	$(".button-page").prepend('<button class="last">&lt;&lt;</button>').append('<button class="last">&gt;&gt;</button>');
+	$(".button-page button").each(function(){
 		if($(this).hasClass("new")){
 			$(this).attr("disabled",true);
 		}
 	})
-	if($(".button-tag-group button").eq(1).hasClass("new")){
-	 	$(".button-tag-group button").eq(0).addClass("no-click no-hover").attr("disabled",true);
+	if($(".button-page button").eq(1).hasClass("new")){
+	 	$(".button-page button").eq(0).addClass("no-click no-hover").attr("disabled",true);
 	 }
-	if($(".button-tag-group button").eq(button_tag_group_length).hasClass("new")){
-		$(".button-tag-group button").last().addClass("no-click no-hover").attr("disabled",true);
+	if($(".button-page button").eq(button_page_length).hasClass("new")){
+		$(".button-page button").last().addClass("no-click no-hover").attr("disabled",true);
 	}
 })
 
@@ -126,3 +126,67 @@ $(document).ready(function() {
 		}
 	})
 });
+
+//footer页脚脚本
+$(document).ready(function(){
+	$(".web-footer").wrapInner("<div class='inner'></div>");
+	$(".web-footer2").wrapInner("<div class='inner'></div>");
+	$(".web-footer3").wrapInner("<div class='inner'></div>");
+	$(".web-footer3-bottom").wrapInner("<div class='inner'></div>");
+	$(".web-footer").find(".inner").each(function(){
+		var footer_left = $(this).find("dl").size();
+		 if(footer_left == 0){
+			$(this).find(".footer-int").removeClass("footer-int").css("color","#428bca");
+		}else if(footer_left == 1){
+			$(this).find(".footer-int").css("margin-left","105px")
+		}else if(footer_left > 1){
+			$(this).find(".footer-int").css({
+				"margin-left":((footer_left*1-1)*145)+105 + "px"
+			})
+		}
+	})
+	$(".web-footer2").find(".inner").each(function(){
+		$(this).find("ul").eq(1).find("img").eq(0).hover(function(){
+			$(this).attr("src","/img/footer-qq2.png");
+		},function(){
+			$(this).attr("src","/img/footer-qq.png");
+		})
+		$(this).find("ul").eq(1).find("img").eq(1).hover(function(){
+			$(this).attr("src","/img/footer-weibo2.png");
+		},function(){
+			$(this).attr("src","/img/footer-weibo.png");
+		})
+		$(this).find("ul").eq(1).find("img").eq(2).hover(function(){
+			$(this).attr("src","/img/footer-qqweibo2.png");
+		},function(){
+			$(this).attr("src","/img/footer-qqweibo.png");
+		})
+		$(this).find("ul").eq(1).find("img").eq(3).hover(function(){
+			$(this).attr("src","/img/footer-weixin2.png");
+		},function(){
+			$(this).attr("src","/img/footer-weixin.png");
+		})
+	})
+	$(".web-footer3-bottom").find(".inner").each(function(){
+		$(this).find("ul").find("img").eq(0).hover(function(){
+			$(this).attr("src","/img/footer-qq2-32.png");
+		},function(){
+			$(this).attr("src","/img/footer-qq-32.png");
+		})
+		$(this).find("ul").find("img").eq(1).hover(function(){
+			$(this).attr("src","/img/footer-weibo2-32.png");
+		},function(){
+			$(this).attr("src","/img/footer-weibo-32.png");
+		})
+		$(this).find("ul").find("img").eq(2).hover(function(){
+			$(this).attr("src","/img/footer-qqweibo2-32.png");
+		},function(){
+			$(this).attr("src","/img/footer-qqweibo-32.png");
+		})
+		$(this).find("ul").find("img").eq(3).hover(function(){
+			$(this).attr("src","/img/footer-weixin2-32.png");
+		},function(){
+			$(this).attr("src","/img/footer-weixin-32.png");
+		})
+	})
+})
